8071ce7d61a75e269f1e927be2369dc5bd49b3b1
Andrew Randan <andrewrandan@gmail.com>
2022-09-16 11:15:58 +0300

    added flowers

--- cron/index.ts
+++ cron/index.ts
@@ -10,4 +10,5 @@ const cronOptions = {
 
 cron.schedule('0 10 * * *', () => {
   sendPhotoFromStock(Number(chatId), 'penis', '@irinamysiuk, увімкни уяву і не забувай, що тебе тут чекають =)');
+  sendPhotoFromStock(Number(chatId), 'flowers', 'Квіти ;)');
 }, cronOptions);
